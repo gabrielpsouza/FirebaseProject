91cc36429eac709a7995bc558a62bdd9e66f99f0
Gabriel Pedro Souza <gabriel.souza@corp.eptv>
2019-08-21 18:14:18 -0300

    primeiro commit

---
+++ src/app/contatos/edit/edit.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Contato } from "../shared/contato";
+import { ContatoService } from "../shared/contato.service";
+import { ContatoDataService } from "../shared/contato-data.service";
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
+})
+export class EditComponent implements OnInit {
+  contato: Contato
+  key: string = '';
+
+  constructor(private contatoService: ContatoService, private contatoDataServic: ContatoDataService) { }
+
+  ngOnInit() {
+    this.contato = new Contato();
+  }
+
+  onSubmit(){
+    // this.contatoService
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {  
+    apiKey: "AIzaSyAqTHMEhObzKbcQWQ_ieqjaIIHhk_3T0oU",
+    authDomain: "fir-project-6bb2c.firebaseapp.com",
+    databaseURL: "https://fir-project-6bb2c.firebaseio.com",
+    projectId: "fir-project-6bb2c",
+    storageBucket: "",
+    messagingSenderId: "28056063310",
+    appId: "1:28056063310:web:ecf6ad81b5389d58"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
